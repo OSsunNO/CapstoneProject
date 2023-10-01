9b9d1ef2eddf46b77aaddd51218bfbc3e6b3f80e
OSsunNO <1020sunho@naver.com>
2023-10-02 02:02:22 +0900

    Upgrading spc module complete
    modify the dictionary file name from emoticon_dict_v1 to spc_dict_v2
    
    comment lines are modified
    
    modifying ':' processing
    
    Signed-off-by: OSsunNO <1020sunho@naver.com>

--- dt-demo-be/modules/spc/run.py
+++ dt-demo-be/modules/spc/run.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import time
 import emoji
+import re
 
 
 def run(fileName):
@@ -14,14 +15,14 @@ def run(fileName):
       for t in test_data['document']:
             line = t.split('\n')
             for l in line:
-                  fd.write(l)
+                  fd.write(l+'\n')
     fd.close()
     
     # module processing start
     start = time.time()
 
-    # Read spc_dict
-    spc_dict = pd.read_excel("modules/spc/spc_dict_v1.xlsx")
+    # Read emoticon_dict
+    emoticon_dict = pd.read_excel("modules/spc/spc_dict_v2.xlsx")
   
     # Find input file from database directory
     db_dir = os.path.join("..", "database")
@@ -35,10 +36,38 @@ def run(fileName):
         with open(file_dir, "r", encoding='utf-8') as f:
             for line in f:
                 target=line.strip()
-                # TODO: emoji logic if-else
+                
+                detected_colon=[':']*target.count(':')
+                if(len(detected_colon)!=0):
+                    print(f"detected_spc: {detected_colon}, converted_text_form: '', desc: spc")
+                target=target.replace(':','')
+                    
+                
+                # EMOJI
+                # print out detected_emoji and converted_text_form
+                a=list(emoji.analyze(target))
+                for i in range(len(a)):
+                    print(f"detected_emoji: {a[i].chars}, converted_text_form: {emoji.demojize(a[i].chars)}, desc: emoji") # print out the log
+                # let emoji convert into text form
                 target=emoji.demojize(target)
-                for s in spc_dict['spc']:
-                    target = target.replace(s, '')
+                
+                # EMOTICON
+                for e, c in zip(emoticon_dict['emoticon'], emoticon_dict['category']):
+                    if e in target:
+                        print(f"detected_emoticon: {e}, converted_text_form: {c}, desc: emoticon")  # print out the log
+                        target = target.replace(e, ':'+c+':')  # replace the emoticon with :category:
+                
+                
+                # 1-digit SPC
+                special=re.compile(r'[^\sㄱ-ㅣA-Za-z0-9가-힣:+]')
+                # find 1-digit special character in target sentence
+                detected_spc = special.findall(target)
+                if(len(detected_spc)!=0):
+                    print(f"detected_spc: {detected_spc}, converted_text_form: '', desc: spc")
+                # remove 1-digit special character
+                target=special.sub('',target)
+                
+                
                 nf.write(target+'\n')
         f.close()
     nf.close()
