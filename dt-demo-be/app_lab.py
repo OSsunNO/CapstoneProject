e713e3c1db9ded707cdf2dba8b1fd3169e9ccbe1
doorooful <codingdooroo@gmail.com>
2023-10-05 19:17:39 +0900

    Get name of uploaded file list
    - get File name from doc_list
    - getter for Document class
    
    Signed-off-by: doorooful <codingdooroo@gmail.com>

--- dt-demo-be/app_lab.py
+++ dt-demo-be/app_lab.py
@@ -11,12 +11,14 @@ did = 1
 created_doc = Document(app, did)
 
 test_doc = None # This is user-uploaded file
+doc_list = []
 
 @app.route("/api/filter/upload", methods=["POST"])
 def file_upload():
     app.logger.info("API request on upload func: POST")
     uploaded_status = "FAIL"
     isFileExists = 'file' in request.files and request.files['file'] is not None
+    # TODO: upload multiple numbers of files
     if isFileExists:
         file = request.files['file']
         contents = file.read().decode('utf-8')
@@ -26,12 +28,24 @@ def file_upload():
         uploaded_status = test_doc.upload(file.filename, contents, db_conn)
         db_conn.close()
         
+        doc_list.append(test_doc)
     return jsonify({'result': uploaded_status})
 
 # TODO: Design API for saving selected modules
 @app.route("/api/filter/modules", methods=["POST"])
 def post_selected_modules():
     return "post_selected_modules"
+
+@app.route("/api/filter/filelist", methods=["GET"])
+def file_list():
+    app.logger.info("API request on upload func: GET")
+    user_uploaded_file_list = [d.getDname() for d in doc_list]
+
+    # TODO: remove me, or use only for developing purpose
+    # file_list_sample = ["file1", "file2", "file3", "file4", "new file"]
+    # return jsonify(file_list_sample)
+
+    return jsonify(user_uploaded_file_list)
 
 @app.route("/api/filter/detreport", methods=["GET"])
 def get_det_report():
